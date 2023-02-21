37673af9a93953ed686ac5545541cfa8f82cca8e
LuciusGabriel <lucius_gabriel@hotmail.com>
2023-02-20 23:18:59 -0300

    finaliza icons

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,22 @@ instagram.addEventListener('mouseover', ()=>{
 instagram.addEventListener('mouseout', ()=>{
     instagram.src = 'assets/img/icons/instagram-pb.svg'
 })
+
+
+const linkedin = document.querySelector('.linked')
+linkedin.addEventListener('mouseover', ()=>{
+    linkedin.src = 'assets/img/icons/linkedin-color.svg'
+    console.log('Passou')
+})
+linkedin.addEventListener('mouseout', ()=>{
+    linkedin.src = 'assets/img/icons/linkedin-pb.svg'
+})
+
+const github = document.querySelector('.git')
+github.addEventListener('mouseover', ()=>{
+    github.src = 'assets/img/icons/github-color.svg'
+    console.log('Passou')
+})
+github.addEventListener('mouseout', ()=>{
+    github.src = 'assets/img/icons/github-pb.svg'
+})
